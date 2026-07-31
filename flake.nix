2c55981595170631f2f9d5395ffe44dd94c47b85
Christian Ulrich <christian@ulrich.earth>
2026-07-31 23:17:12 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems (
+        system:
+        let
+          overlays = [ ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          pkgs-unstable = import nixpkgs-unstable { inherit system overlays; };
+        in
+        pkgs.mkShell {
+          name = "javascript devshell";
+
+          buildInputs = with pkgs; [
+            nodejs_26
+            vscode-langservers-extracted
+            typescript-language-server
+            prettierd
+          ];
+        }
+      );
+    };
+}
