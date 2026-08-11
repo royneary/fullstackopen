20ad1b3817deeb6a0d2fbea49ffc8dbcaddbb9df
Christian Ulrich <christian@ulrich.earth>
2026-08-11 21:07:28 +0200

    exercise 4.15

--- part-04/bloglist/app.js
+++ part-04/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 
 const app = express();
@@ -11,6 +12,8 @@ mongoose.connect(config.MONGODB_URI, { family: 4 });
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 
