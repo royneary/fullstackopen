b3db32a2e56abb6c882f3f5bdd58e7ec0007bdb8
Christian Ulrich <christian@ulrich.earth>
2026-08-12 14:58:13 +0200

    exercise 4.17

--- part-04/bloglist/models/user.js
+++ part-04/bloglist/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, minlength: 3, unique: true },
   name: { type: String, required: true },
   passwordHash: { type: String, required: true },
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }],
 });
 
 userSchema.set("toJSON", {
