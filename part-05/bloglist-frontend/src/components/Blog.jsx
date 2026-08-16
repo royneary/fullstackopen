76b1b4a987806daa27d4d6b3528d1b0256d85c55
Christian Ulrich <christian@ulrich.earth>
2026-08-16 19:51:10 +0200

    exercise 5.7

--- part-05/bloglist-frontend/src/components/Blog.jsx
+++ part-05/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const buttonLabel = visible ? "hide" : "view";
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setVisible(!visible)}>{buttonLabel}</button>
+      {visible ? (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Blog;
