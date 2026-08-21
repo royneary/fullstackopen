f10da35c63d89552d7cd6c35b11e70558938204b
Christian Ulrich <christian@ulrich.earth>
2026-08-21 23:17:41 +0200

    exercise 6.7

--- part-06/zustand-anecdotes/src/store.js
+++ part-06/zustand-anecdotes/src/store.js
@@ -1,13 +1,5 @@
 import { create } from "zustand";
-
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
+import anecdotesService from "./services/anecdotes";
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -20,22 +12,29 @@ const asObject = (anecdote) => ({
 const compareByVotes = (a1, a2) => a2.votes - a1.votes;
 
 const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
   filter: "",
   actions: {
+    initialize: async () => {
+      const anecdotes = await anecdotesService.getAll();
+      set(() => ({ anecdotes }));
+    },
+
     vote: (id) =>
       set((state) => ({
         anecdotes: state.anecdotes
           .map((a) => (a.id === id ? { ...a, votes: a.votes + 1 } : a))
           .toSorted(compareByVotes),
       })),
+
     add: ({ content, votes }) =>
       set((state) => ({
         anecdotes: state.anecdotes
           .concat({ id: getId(), content, votes })
           .toSorted(compareByVotes),
       })),
-    setFilter: (filter) => set((state) => ({ filter })),
+
+    setFilter: (filter) => set(() => ({ filter })),
   },
 }));
 
