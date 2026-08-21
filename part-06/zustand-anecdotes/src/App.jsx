4f6aa3accf0a7a4194a3028e25b74012ee7366cf
Christian Ulrich <christian@ulrich.earth>
2026-08-21 16:00:55 +0200

    exercise 6.4

--- part-06/zustand-anecdotes/src/App.jsx
+++ part-06/zustand-anecdotes/src/App.jsx
@@ -1,35 +1,12 @@
-import { useAnecdoteActions, useAnecdotes } from "./store";
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useAnecdotes();
-  const { vote, add } = useAnecdoteActions();
-
-  const handleCreate = (e) => {
-    e.preventDefault();
-    const content = e.target.anecdote.value;
-    add({ content, votes: 0 });
-    e.target.reset();
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   );
 };
---
+++ part-06/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useAnecdoteActions } from "../store";
+
+const AnecdoteForm = () => {
+  const { add } = useAnecdoteActions();
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+    const content = e.target.anecdote.value;
+    add({ content, votes: 0 });
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
