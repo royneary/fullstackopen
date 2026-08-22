ec74a8b229e1249d33831c640d97c85cf26198e7
Christian Ulrich <christian@ulrich.earth>
2026-08-22 19:15:57 +0200

    exercise 7.1

---
+++ part-07/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
+
+const CreateNew = ({ addAnecdote }) => {
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addAnecdote({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    });
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name="content" {...content} />
+        </div>
+        <div>
+          author
+          <input name="author" {...author} />
+        </div>
+        <div>
+          url for more info
+          <input name="info" {...info} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateNew;
