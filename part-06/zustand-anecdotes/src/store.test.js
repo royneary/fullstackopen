d8811a1837a5bad848391ee056767c8258c51cc7
Christian Ulrich <christian@ulrich.earth>
2026-08-22 11:33:35 +0200

    use common data for all tests

--- part-06/zustand-anecdotes/src/store.test.js
+++ part-06/zustand-anecdotes/src/store.test.js
@@ -3,6 +3,25 @@ import useAnecdoteStore, { useAnecdotes, useAnecdoteActions } from "./store";
 import anecdotesService from "./services/anecdotes";
 import { renderHook, act } from "@testing-library/react";
 
+const anecdotes = [
+  {
+    content: "If it hurts, do it more often",
+    id: "47145",
+    votes: 3,
+  },
+  {
+    content: "Adding manpower to a late software project makes it later!",
+    id: "21149",
+    votes: 15,
+  },
+  {
+    content:
+      "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    id: "69581",
+    votes: 4,
+  },
+];
+
 vi.mock("./services/anecdotes.js", () => ({
   default: {
     getAll: vi.fn(),
@@ -18,25 +37,7 @@ beforeEach(() => {
 });
 
 test("initialize loads anecdotes from service", async () => {
-  const mockAnecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 0,
-    },
-    {
-      content: "Adding manpower to a late software project makes it later!",
-      id: "21149",
-      votes: 0,
-    },
-    {
-      content:
-        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-      id: "69581",
-      votes: 0,
-    },
-  ];
-  anecdotesService.getAll.mockResolvedValue(mockAnecdotes);
+  anecdotesService.getAll.mockResolvedValue(anecdotes);
 
   const { result } = renderHook(() => useAnecdoteActions());
 
@@ -45,29 +46,13 @@ test("initialize loads anecdotes from service", async () => {
   });
 
   const { result: anecdotesResult } = renderHook(() => useAnecdotes());
-  expect(anecdotesResult.current).toEqual(mockAnecdotes);
+
+  anecdotes.forEach((anecdote) => {
+    expect(anecdotesResult.current).toContainEqual(anecdote);
+  });
 });
 
 test("anecdotes are returned sorted by votes", async () => {
-  const anecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 3,
-    },
-    {
-      content: "Adding manpower to a late software project makes it later!",
-      id: "21149",
-      votes: 15,
-    },
-    {
-      content:
-        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-      id: "69581",
-      votes: 4,
-    },
-  ];
-
   useAnecdoteStore.setState({ anecdotes });
 
   const { result } = renderHook(() => useAnecdotes());
