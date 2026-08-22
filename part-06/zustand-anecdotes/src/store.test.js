8143d812d5cfdf0fd4a7f30a6db8ef1c4c32c9e8
Christian Ulrich <christian@ulrich.earth>
2026-08-22 11:09:40 +0200

    exercise 6.13

--- part-06/zustand-anecdotes/src/store.test.js
+++ part-06/zustand-anecdotes/src/store.test.js
@@ -47,3 +47,30 @@ test("initialize loads anecdotes from service", async () => {
   const { result: anecdotesResult } = renderHook(() => useAnecdotes());
   expect(anecdotesResult.current).toEqual(mockAnecdotes);
 });
+
+test("anecdotes are returned sorted by votes", async () => {
+  const anecdotes = [
+    {
+      content: "If it hurts, do it more often",
+      id: "47145",
+      votes: 3,
+    },
+    {
+      content: "Adding manpower to a late software project makes it later!",
+      id: "21149",
+      votes: 15,
+    },
+    {
+      content:
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      id: "69581",
+      votes: 4,
+    },
+  ];
+
+  useAnecdoteStore.setState({ anecdotes });
+
+  const { result } = renderHook(() => useAnecdotes());
+
+  expect(result.current.map((a) => a.votes)).toEqual([15, 4, 3]);
+});
